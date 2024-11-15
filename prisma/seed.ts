7f674821600e1596f3f7c4d3597e8d4863e92778
Adam Hao <haozg44@outlook.com>
2024-11-15 22:52:24 +0800

    utilize fakerJS to generate dummy database records

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,50 +1,28 @@
-import { Prisma, PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+
+var vocabEntries: Prisma.VocabEntryCreateInput[] = [];
+let uniqueSentences = faker.helpers.uniqueArray(faker.lorem.sentence, 50);
+
+for (let index = 0; index < uniqueSentences.length; index++) {
+	vocabEntries.push({
+		sentence: uniqueSentences[index],
+		sentencePlusPhoneticSymbols: uniqueSentences[index],
+		translation: `这是测试翻译${index}`,
+		note: faker.lorem.sentences(),
+		user: {
+			connect: {
+				email: 'adam@gmail.com',
+			},
+		},
+	});
+}
 
 const prisma = new PrismaClient();
 
-var initialVocabSheet: Prisma.VocabEntryCreateInput[] = [
-	{
-		sentence: 'sentence one',
-		sentencePlusPhoneticSymbols: 'sentence one',
-		translation: '翻译一',
-		user: {
-			connectOrCreate: {
-				where: {
-					email: 'adam@gmail.com',
-				},
-				create: {
-					email: 'adam@gmail.com',
-					name: 'adam hao',
-					password: '123456',
-				},
-			},
-		},
-	},
-	{
-		sentence: 'sentence two',
-		sentencePlusPhoneticSymbols: 'sentence two',
-		translation: '翻译二',
-		user: {
-			connect: {
-				email: 'adam@gmail.com',
-			},
-		},
-	},
-	{
-		sentence: 'sentence three',
-		sentencePlusPhoneticSymbols: 'sentence three',
-		translation: '翻译三',
-		user: {
-			connect: {
-				email: 'adam@gmail.com',
-			},
-		},
-	},
-];
-
 async function main() {
 	console.log('Start seeding ...');
-	for (const vocabEntry of initialVocabSheet) {
+	for (const vocabEntry of vocabEntries) {
 		const newEntry = await prisma.vocabEntry.create({ data: vocabEntry });
 		console.log(`Created post with id: ${newEntry.id}`);
 		console.log('Seeding finished.');
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Vocab from '@/components/Vocab';
 export default function Home() {
 	return (
 		<React.Suspense fallback='Loading...'>
-			{/* TODO ~~a skeleton loading component for the fallback, which includes an input box~~ maybe I don't need a suspense here after all */}
+			{/* TODO a skeleton loading component for the fallback, which includes an input box*/}
 			<Vocab />
 		</React.Suspense>
 	);
